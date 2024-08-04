f2d61f614b3b7080a190797cdda2f7d7c443c158
Riyaz Maderbocus <madsonray0@gmail.com>
2024-08-04 18:33:44 +0100

    update ignore file

--- public/form.js
+++
@@ -1,9 +0,0 @@
-// alert("hello");
-
-const form = document.querySelector("#form");
-
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    // alert("form halted");
-    console.log("halt form");
-})
